3f94052b3c3f1e139b578e285ddd24f80f27282f
Damian Barabonkov <db.htam@gmail.com>
2023-02-11 23:15:42 +0100

    Code comments

--- solutions/day_1/solution.py
+++ solutions/day_1/solution.py
@@ -3,7 +3,10 @@ from typing import TextIO
 
 TEXT_FILE_LOCATION = "input.txt"
 
-
+# DB So, the way you are using the `open_file`, you never close the file.
+# This is bad practice. The prefered way to deal with files in Python is
+# with a `with` clause actually. Look into using that instead. The `with`
+# clause automatically closes the file once you exit the clause.
 def open_file(file: str) -> TextIO:
     f = open(TEXT_FILE_LOCATION, "r")
     return f
@@ -13,16 +16,31 @@ def open_file(file: str) -> TextIO:
 # if it's new line take the sum and create that as new value using incrementing value as key
 # othewise add the value in the line to the sum and keep going
 def sum_elf_food(file_location: TextIO) -> dict:
+    # DB: I am curios why you begin the counter at `1` and not `0`
     elf_number = 1
     calorie_sum = 0
     dict_of_cals = {}
+    
+    # DB: Unsure where `file` comes from. It is not defined anywhere.
     for line in file:
+        # DB: For this type of structure, use a set (ie: {"\n", "\r\n"}) is faster
+        # than a tuple lookup.
+        # DB: I am also going to re-write this, keeping the structure you used, but
+        # using dicts in a more clever manner
+        """
         if line in ("\n", "\r\n"):
             dict_of_cals[elf_number] = calorie_sum
             calorie_sum = 0
             elf_number += 1
         else:
             calorie_sum += int(line)
+        """
+        if line in {"\n", "\r\n"}:
+            # Move on to the next elf
+            elf_number += 1
+        else:
+            # The `get` does a query, but can supply a default value if the key does not exist
+            dict_of_cals[elf_number] = dict_of_cals.get(elf_number, 0) + int(line)
     return dict_of_cals
 
 
