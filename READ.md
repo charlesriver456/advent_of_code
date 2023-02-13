c2178e8be8e50df995afbbb2e588f79d0916caac
charlesriver456 <none>
2023-02-13 15:48:33 -0500

    black changes

---
+++ READ.md
@@ -0,0 +1,5 @@
+##Solutions to Advent of Code
+
+Link to the site: https://adventofcode.com
+
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
---
+++ solutions/day_3/solution.py
@@ -0,0 +1,46 @@
+from typing import List
+from functools import reduce
+
+TEXT_FILE_LOCATION = "input.txt"
+LOWER_CASE_ORD_DIFFERENCE = 96
+UPPER_CASE_ORD_DIFFERENCE = 38
+
+
+def find_intersection(acc: List, val: map) -> List[int]:
+    if val != "":
+        midpoint = len(val) // 2
+        char_to_add = set(val[:midpoint]).intersection(set(val[midpoint:])).pop()
+        if char_to_add.islower():
+            acc.append(ord(char_to_add) - LOWER_CASE_ORD_DIFFERENCE)
+        else:
+            acc.append(ord(char_to_add) - UPPER_CASE_ORD_DIFFERENCE)
+    else:
+        acc += []
+    return acc
+
+
+def create_groups(acc: List[List], val: map) -> List[List]:
+    print("reached")
+    iterator = 0
+    if val != "" and iterator % 3 != 0 and iterator > 0:
+        acc.append(val)
+        iterator += 1
+    else:
+        acc += [[]]
+        acc.append(val)
+        iterator += 1
+    return acc
+
+
+def main():
+    with open(TEXT_FILE_LOCATION) as file:
+        lines_part_1 = map(lambda s: s.rstrip("\n"), file.readlines())
+        lines_part_2 = map(lambda s: s.rstrip("\n"), file.readlines())
+    elf_item_list = reduce(find_intersection, lines_part_1, [])
+    group_list = reduce(create_groups, lines_part_2, [[]])
+    print(group_list)
+    print(f"Sum for part one is: {sum(elf_item_list)}")
+
+
+if __name__ == "__main__":
+    main()
