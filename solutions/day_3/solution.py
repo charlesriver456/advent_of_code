76dc11389adbf9174e63c447c35fa770fbd78904
charlesriver456 <none>
2023-02-15 22:18:49 -0500

    black changes

--- solutions/day_3/solution.py
+++ solutions/day_3/solution.py
@@ -6,7 +6,7 @@ LOWER_CASE_ORD_DIFFERENCE = 96
 UPPER_CASE_ORD_DIFFERENCE = 38
 
 
-def find_intersection(acc: List, val: map) -> List[int]:
+def find_intersection(acc: List, val: List[str]) -> List[int]:
     if val != "":
         midpoint = len(val) // 2
         char_to_add = set(val[:midpoint]).intersection(set(val[midpoint:])).pop()
@@ -19,26 +19,23 @@ def find_intersection(acc: List, val: map) -> List[int]:
     return acc
 
 
-def create_groups(acc: List[List], val: map) -> List[List]:
-    print("reached")
-    iterator = 0
-    if val != "" and iterator % 3 != 0 and iterator > 0:
-        acc.append(val)
-        iterator += 1
+def create_groups(acc: List[List], val: List[str]) -> List[int]:
+    char_to_add = set(val[0]).intersection(set(val[1]).intersection(set(val[2]))).pop()
+    if char_to_add.islower():
+        acc.append(ord(char_to_add) - LOWER_CASE_ORD_DIFFERENCE)
     else:
-        acc += [[]]
-        acc.append(val)
-        iterator += 1
+        acc.append(ord(char_to_add) - UPPER_CASE_ORD_DIFFERENCE)
     return acc
 
 
 def main():
     with open(TEXT_FILE_LOCATION) as file:
-        lines = map(lambda s: s.rstrip("\n"), file.readlines())
+        lines = list(map(lambda s: s.rstrip("\n"), file.readlines()))
     elf_item_list = reduce(find_intersection, lines, [])
-    group_list = reduce(create_groups, lines, [[]])
-    print(group_list)
+    final = list(zip(*[iter(lines)] * 3))
+    group_list = reduce(create_groups, final, [])
     print(f"Sum for part one is: {sum(elf_item_list)}")
+    print(f"Sum for part two is: {sum(group_list)}")
 
 
 if __name__ == "__main__":
