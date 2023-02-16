08d8f6fb87534e9581018c14614fbb59a0dfeaae
charlesriver456 <none>
2023-02-16 16:20:58 -0500

    black changes

--- solutions/day_3/solution.py
+++ solutions/day_3/solution.py
@@ -6,20 +6,22 @@ LOWER_CASE_ORD_DIFFERENCE = 96
 UPPER_CASE_ORD_DIFFERENCE = 38
 
 
-def find_intersection(acc: List, val: List[str]) -> List[int]:
+def ord_calculate_helper(char: str) -> int:
+    if char.islower():
+        return ord(char) - LOWER_CASE_ORD_DIFFERENCE
+    else:
+        return ord(char) - UPPER_CASE_ORD_DIFFERENCE
+
+
+def find_intersection(acc: List[int], val: List[str]) -> List[int]:
     if val != "":
         midpoint = len(val) // 2
         char_to_add = set(val[:midpoint]).intersection(set(val[midpoint:])).pop()
-        if char_to_add.islower():
-            acc.append(ord(char_to_add) - LOWER_CASE_ORD_DIFFERENCE)
-        else:
-            acc.append(ord(char_to_add) - UPPER_CASE_ORD_DIFFERENCE)
-    else:
-        acc += []
+        acc.append(ord_calculate_helper(char_to_add))
     return acc
 
 
-def calculate_letter_value(acc: List, val: List[str]) -> List[int]:
+def calculate_letter_value(acc: List[int], val: List[str]) -> List[int]:
     char_to_add = set(val[0]).intersection(set(val[1]).intersection(set(val[2]))).pop()
     if char_to_add.islower():
         acc.append(ord(char_to_add) - LOWER_CASE_ORD_DIFFERENCE)
@@ -32,6 +34,7 @@ def main():
     with open(TEXT_FILE_LOCATION) as file:
         lines = list(map(lambda s: s.rstrip("\n"), file.readlines()))
     elf_item_list = reduce(find_intersection, lines, [])
+    # creates iter object and copies it twice over (3 total elements)
     final = list(zip(*[iter(lines)] * 3))
     group_list = reduce(calculate_letter_value, final, [])
     print(f"Sum for part one is: {sum(elf_item_list)}")
--- solutions/day_4/solution.py
+++ solutions/day_4/solution.py
@@ -7,28 +7,10 @@ TEXT_FILE_LOCATION = "input.txt"
 def calculate(acc: List, val: List[str]) -> List[int]:
     # if first pair covers second pair
     if int(val[0]) <= int(val[2]) and int(val[1]) >= int(val[3]):
-        acc.append(
-            "first bigger than second, "
-            + val[0]
-            + ","
-            + val[1]
-            + " versus "
-            + val[2]
-            + ","
-            + val[3]
-        )
+        acc.append(1)
     # if second pair covers first pair
-    if int(val[2]) <= int(val[0]) and int(val[3]) >= int(val[1]):
-        acc.append(
-            "second bigger than first, "
-            + val[2]
-            + ","
-            + val[3]
-            + " versus "
-            + val[0]
-            + ","
-            + val[1]
-        )
+    elif int(val[2]) <= int(val[0]) and int(val[3]) >= int(val[1]):
+        acc.append(1)
     else:
         acc.append(0)
     return acc
@@ -39,9 +21,7 @@ def main():
         lines = map(lambda s: s.rstrip("\n"), file.readlines())
         lines = list(map(lambda s: s.replace(",", "-").split("-"), lines))
     number_list = reduce(calculate, lines, [])
-    print(lines)
-    print(number_list)
-    # print(f"Sum for part one is: {sum(number_list)}")
+    print(f"Sum for part one is: {sum(number_list)}")
 
 
 if __name__ == "__main__":
